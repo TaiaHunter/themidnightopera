8082b380e91a501bae8247d5d452dd8c57043932
Erallie <eds.gozar@outlook.com>
2025-05-07 10:00:37 -0700

    Copied About contents from website

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,15 +4,61 @@
 </script>
 
 <SingleImage source="/opera-info.png" title="Opera Info" width={300} />
-<TextBox
-	><p>
-		The Midnight Opera is an adults only vrchat group that focuses on social events and live
-		performances ranging from acting and poetry to singing and dancing. If it can happen on a stage,
-		it can happen here. We are quest compatible and optimized, with bar nights, ballroom events,
-		live performances with stage sets and more.
-	</p>
+
+<TextBox>
 	<p>
-		Our goal?<br />
-		To reach for the stars. That burning passion to grow and improve and that spark to entertain.
+		The Midnight Opera is an adult-only VRChat group that focuses on social events and live
+		performances, ranging from acting and poetry to singing and dancing.
 	</p>
 </TextBox>
+<h1>Our Features</h1>
+<TextBox>
+	<div class="grid">
+		<div>
+			<h1>Open Mic Nights</h1>
+			<p>
+				Want to make your voice heard? Come and perform at one of our Open Mic nights! We host one
+				every other Saturday at 7pm CST.
+			</p>
+		</div>
+		<div>
+			<h1>Themed Bar and Music Nights</h1>
+			<p>
+				Join us every other Wednesday at 7pm CST for a themed bar night with music from the 50’s,
+				musicals, and more.
+			</p>
+		</div>
+		<div>
+			<h1>Spotlight Solo Performances</h1>
+			<p>
+				Sometimes there’s just a talent that’s so good we need to give them the spotlight. Join us
+				every other Friday at 7pm CST for an amazing show by even more amazing people.
+			</p>
+		</div>
+		<div>
+			<h1>Movie Nights</h1>
+			<p>
+				From time to time, we’ll all get together and watch a movie during our bar night that’s also
+				in theme with the main event.
+			</p>
+		</div>
+		<div>
+			<h1>Stage Productions</h1>
+			<p>Live music shows, improv shows and whatever else requires a stage and performing!</p>
+		</div>
+		<div>
+			<h1>Quest Compatibility and Optimization</h1>
+			<p>
+				All virtual venues are Quest compatible and optimized. Lack of crashing and questies see the
+				stage catch fire is a must!
+			</p>
+		</div>
+	</div>
+</TextBox>
+
+<style>
+	.grid {
+		display: grid;
+		grid-template-columns: repeat(3, 1fr);
+	}
+</style>
