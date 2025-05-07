cbe2853e997591c3928b69ec6358830422bc53d0
Erallie <eds.gozar@outlook.com>
2025-05-07 10:01:26 -0700

    Added credits

---
+++ src/routes/credits/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import TextBox from '$lib/components/TextBox.svelte';
+</script>
+
+<h1>Credits</h1>
+<TextBox>
+	<p>
+		Website coded primarily by <a href="https://gozarproductions.com" target="_blank">Erika Gozar</a
+		>.
+	</p>
+	<p>Insert other things like this.</p>
+</TextBox>
