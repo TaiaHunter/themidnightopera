b7e5ada6ddeff4482082351ebdde74697f2947f9
Erallie <eds.gozar@outlook.com>
2025-05-06 18:45:10 -0700

    Made option for columns to not be resizable

--- src/lib/components/Column.svelte
+++ src/lib/components/Column.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
 	import type { ColumnComponents } from '$lib/types';
 
-	let { weight, children }: ColumnComponents = $props();
+	let { weight, children, noResize }: ColumnComponents = $props();
 </script>
 
-<div class="column" style="flex: {weight}">
+<div class="column" style={noResize ? 'flex-grow: 0; flex-shrink: 0' : `flex: ${weight}`}>
 	{@render children()}
 </div>
--- src/lib/types.ts
+++ src/lib/types.ts
@@ -16,6 +16,7 @@ export type SlideshowComponents = {
 };
 
 export type ColumnComponents = {
-	weight: number;
+	weight?: number;
 	children: any;
+	noResize?: boolean;
 };
