e68652ae01d240585a218527ec24e24c5d9ab685
Erallie <eds.gozar@outlook.com>
2025-08-20 11:24:01 -0700

    Build Website

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,16 @@
-import { mdsvex } from 'mdsvex';
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
-	preprocess: [vitePreprocess(), mdsvex()],
-	kit: { adapter: adapter() },
-	extensions: ['.svelte', '.svx']
+	kit: {
+		adapter: adapter(),
+		paths: {
+			// If your repo is a project page (username.github.io/repo-name)
+			// set this to '/repo-name'. If it's a user/organization page, leave it as ''.
+			base: process.env.NODE_ENV === 'production' ? '/themidnightopera' : ''
+		}
+	},
+	preprocess: vitePreprocess()
 };
 
 export default config;
