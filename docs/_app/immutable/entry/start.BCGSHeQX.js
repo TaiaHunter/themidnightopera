917275f15c1f52fc9871760d83e6bdba52514166
TaiaHunter <taiahunterproductions@gmail.com>
2025-08-28 11:31:42 -0500

    rawr

--- docs/_app/immutable/entry/start.BCGSHeQX.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/BZCq42JB.js";export{o as load_css,r as start};
--- src/routes/donations-and-more/+page.svelte
+++ src/routes/donations-and-more/+page.svelte
@@ -6,26 +6,15 @@
 	import DoubleBorder from '$lib/components/DoubleBorder.svelte';
 </script>
 
-<h1>Donations and M</h1>
+<h1>Donations and Merch</h1>
 <DoubleBorder>
 	<p>
 		We rely on donations as well as art commissions to try and raise money for all the things we
 		need. Such as new particles, props and eventually adding a VIP area or a second venue. Every
 		dollar helps, so please donate or purchase a commission via the kofi linked below!
 	</p>
+	
 	<p>
 		<Button link="https://ko-fi.com/taiahunterpro" newTab>Visit Our Ko-fi</Button>
 	</p>
 </DoubleBorder>
-
-<DoubleBorder>
-	<p>
-		Another good way to support us is through our merch store! All artwrk was hand drawn by Taia, the founder, herself!
-		We have pins, mugs, stickers and more! The merch store will likely get new things often based on requests. 
-		All purchases directly support our work, as all funds will go back into world/avatar creation and show maintenance!
-	</p>
-	<p>
-		<Button link="https://themidnightopera.printify.me/" newTab>Visit Our Printify Shop</Button>
-	</p>
-</DoubleBorder>
-
