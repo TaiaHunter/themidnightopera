48a2a0ec5990ac237432321eb2d2af223cf53981
TaiaHunter <taiahunterproductions@gmail.com>
2025-08-28 11:44:56 -0500

    added merch area properly

--- src/routes/donations-and-more/+page.svelte
+++ src/routes/donations-and-more/+page.svelte
@@ -6,7 +6,7 @@
 	import DoubleBorder from '$lib/components/DoubleBorder.svelte';
 </script>
 
-<h1>Donations and Merch</h1>
+<h1>Donations</h1>
 <DoubleBorder>
 	<p>
 		We rely on donations as well as art commissions to try and raise money for all the things we
@@ -16,5 +16,19 @@
 	
 	<p>
 		<Button link="https://ko-fi.com/taiahunterpro" newTab>Visit Our Ko-fi</Button>
+	</p>
+</DoubleBorder>
+
+<h1>Merch</h1>
+<DoubleBorder>
+	<p>
+		Official merch of the opera is finally here! Pins, mugs, stickers and more on the way! 
+		All art is done ditally by hand via Taia Hunter, owner and founder of this establishment.
+		If you would like a way to support us but also get something nice out of it, then check out printify shop!
+		100% of the proceeds will go back into world, avatar and show development!
+	</p>
+	
+	<p>
+		<Button link="https://themidnightopera.printify.me/" newTab>Visit Our Printify</Button>
 	</p>
 </DoubleBorder>
