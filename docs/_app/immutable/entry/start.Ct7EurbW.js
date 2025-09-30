2f2c5cde709a8d82945cc901b14786ac6dff9160
TaiaHunter <taiahunterproductions@gmail.com>
2025-09-30 15:13:58 -0500

    update i hope

---
+++ docs/_app/immutable/entry/start.Ct7EurbW.js
@@ -0,0 +1,1 @@
+import{l as o,a as r}from"../chunks/DvI9JEua.js";export{o as load_css,r as start};
--- src/routes/donations-and-more/+page.svelte
+++ src/routes/donations-and-more/+page.svelte
@@ -6,26 +6,29 @@
 	import DoubleBorder from '$lib/components/DoubleBorder.svelte';
 </script>
 
-<h1>Donations and M</h1>
+<h1>Donations</h1>
 <DoubleBorder>
 	<p>
 		We rely on donations as well as art commissions to try and raise money for all the things we
 		need. Such as new particles, props and eventually adding a VIP area or a second venue. Every
 		dollar helps, so please donate or purchase a commission via the kofi linked below!
 	</p>
+	
 	<p>
 		<Button link="https://ko-fi.com/taiahunterpro" newTab>Visit Our Ko-fi</Button>
 	</p>
 </DoubleBorder>
 
+<h1>Merch</h1>
 <DoubleBorder>
 	<p>
-		Another good way to support us is through our merch store! All artwrk was hand drawn by Taia, the founder, herself!
-		We have pins, mugs, stickers and more! The merch store will likely get new things often based on requests. 
-		All purchases directly support our work, as all funds will go back into world/avatar creation and show maintenance!
+		Official merch of the opera is finally here! Pins, mugs, stickers and more on the way! 
+		All art is done ditally by hand via Taia Hunter, owner and founder of this establishment.
+		If you would like a way to support us but also get something nice out of it, then check out printify shop!
+		100% of the proceeds will go back into world, avatar and show development!
 	</p>
+	
 	<p>
-		<Button link="https://themidnightopera.printify.me/" newTab>Visit Our Printify Shop</Button>
+		<Button link="https://themidnightopera.printify.me/" newTab>Visit Our Printify</Button>
 	</p>
 </DoubleBorder>
-
